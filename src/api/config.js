c03a56d54d70c26734b19d72f3b9c27812713338
Sghen <945654877@qq.com>
2019-09-17 21:48:38 +0800

    [!]配置生产地址

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = process.env.NODE_ENV === 'production' ? 'http://49.235.91.138:8088' : 'http://127.0.0.1:8088'
-export const wsUrl = process.env.NODE_ENV === 'production' ? 'ws://49.235.91.138:8088/ws' : 'ws://127.0.0.1:8088/ws'
+export const baseUrl = process.env.NODE_ENV === 'production' ? 'http://www.sghen.cn:8089' : 'http://127.0.0.1:8088'
+export const wsUrl = process.env.NODE_ENV === 'production' ? 'ws://www.sghen.cn:8089/ws' : 'ws://127.0.0.1:8088/ws'
